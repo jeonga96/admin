d6e1f72aeec593117bc3e5dd75d6497fbdde522a
jeonga96 <wazzang96jeonga@gmail.com>
2022-09-20 10:25:13 +0900

    [style] table css

--- src/screens/ListUser.js
+++ src/screens/ListUser.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { FaUserEdit, FaUserCheck } from "react-icons/fa";
+import { BsCheck2 } from "react-icons/bs";
 import { useEffect, useState } from "react";
 import { servicesPostData } from "../Services/importData";
 import { urlUserlist } from "../Services/string";
@@ -33,11 +33,10 @@ export default function ListUser() {
           <table className="commonTable">
             <thead>
               <tr>
-                <th className="widthS">회원번호</th>
+                <th className="widthM">회원번호</th>
                 <th className="widthB">아이디</th>
-                <th className="widthM">회원 권한</th>
+                <th className="widthS">회원 권한</th>
                 <th className="widthM">생성시간</th>
-                <th className="widthS">상세정보</th>
                 <th className="widthS">인증</th>
               </tr>
             </thead>
@@ -55,16 +54,9 @@ export default function ListUser() {
                   </td>
                   <td>{item.createTime.slice(0, 10)}</td>
                   <td>
-                    {item.name ? (
-                      <i>
-                        <FaUserEdit />
-                      </i>
-                    ) : null}
-                  </td>
-                  <td>
                     {item.useFlag ? (
                       <i>
-                        <FaUserCheck />
+                        <BsCheck2 />
                       </i>
                     ) : null}
                   </td>
