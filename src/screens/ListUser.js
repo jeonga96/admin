4b951f353ffd07414061b0bf9b176eda2a048f9a
jeonga96 <wazzang96jeonga@gmail.com>
2022-11-14 15:02:57 +0900

    [Feat] 통합회원 관리 - 계약관리 복수 선택 추가

--- src/screens/ListUser.js
+++ src/screens/ListUser.js
@@ -1,56 +1,29 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { servicesPostData } from "../Services/importData";
-import { urlUserlist } from "../Services/string";
+import { urlUserlist, urlSetUser } from "../Services/string";
 
 import PageButton from "../components/common/PiecePaginationButton";
 import LayoutTopButton from "../components/common/LayoutTopButton";
 import ComponentListUserSearch from "../components/common/ComponentListUserSearch";
 
-function ListInTr({
-  item,
-  setClickedStatus,
-  clickedStatus,
-  setClickedUseFlag,
-  clickedUseFlag,
-}) {
+function ListInTr({ item, setClickedUseFlag, clickedUseFlag }) {
   const [useFlagCk, setUseFlagCk] = useState(false);
-  const [statusCk, setStatusCk] = useState(false);
 
   const checkedItemHandler = (name, id, isChecked) => {
     (() => {
       if (isChecked) {
-        id === "useFlag"
-          ? setClickedUseFlag([...clickedUseFlag, name])
-          : setClickedStatus([...clickedStatus, name]);
-
+        setClickedUseFlag([...clickedUseFlag, name]);
         // 동일한 선택을 할 때 중복 선택되지 않도록 설정
       } else if (!isChecked && clickedUseFlag.includes(name)) {
         setClickedUseFlag(clickedUseFlag.filter((it) => it !== name));
-      } else if (!isChecked && clickedStatus.includes(name)) {
-        setClickedStatus(clickedStatus.filter((it) => it !== name));
       }
     })();
   };
-
   // 체크 이벤트 동작 & 상위 컴포넌트에게 전달하기 위한 이벤트 동작
   const checkHandler = ({ target }) => {
-    if (target.id === "useFlag") {
-      if (clickedStatus.length == 0) {
-        setUseFlagCk(!useFlagCk);
-        checkedItemHandler(target.name, target.id, target.checked);
-      } else {
-        alert("계약관리와 회원상태를 한 번에 수정하실 수 없습니다.");
-      }
-    }
-    if (target.id === "status") {
-      if (clickedUseFlag.length == 0) {
-        setStatusCk(!statusCk);
-        checkedItemHandler(target.name, target.id, target.checked);
-      }
-    } else {
-      alert("계약관리와 회원상태를 한 번에 수정하실 수 없습니다.");
-    }
+    setUseFlagCk(!useFlagCk);
+    checkedItemHandler(target.name, target.id, target.checked);
   };
 
   return (
@@ -58,27 +31,26 @@ function ListInTr({
       <td>
         <input
           type="checkbox"
-          name={item.cid}
+          name={item.uid}
           checked={useFlagCk}
           id="useFlag"
           onChange={checkHandler}
         />
       </td>
-      <td>
-        <input
-          type="checkbox"
-          name={item.cid}
-          checked={statusCk}
-          id="status"
-          onChange={checkHandler}
-        />
-      </td>
-
-      <td>{item.cid}</td>
+      <td>{item.uid}</td>
+      <td>{item.userid}</td>
       <td>{item.name}</td>
+      <td>
+        {item.userrole && item.userrole.includes("ROLE_ADMIN")
+          ? "관리자"
+          : "일반"}
+      </td>
+      <td>{item.mobile}</td>
       <td>{item.createTime && item.createTime.slice(0, 10)}</td>
+      <td>{item.udid ? <i className="tableIcon">입력</i> : null}</td>
+      <td>{item.cid ? <i className="tableIcon">사업자</i> : null}</td>
       <td className="tableButton">
-        <Link to={`${item.cid}`} className="Link">
+        <Link to={`${item.uid}`} className="Link">
           상세
         </Link>
       </td>
@@ -90,6 +62,7 @@ export default function ListUser() {
   const [userList, setUserList] = useState([]);
   const [listPage, setListPage] = useState({});
   const [page, setPage] = useState({ getPage: 0, activePage: 1 });
+  const [clickedUseFlag, setClickedUseFlag] = useState([]);
 
   useEffect(() => {
     servicesPostData(urlUserlist, {
@@ -101,6 +74,26 @@ export default function ListUser() {
     });
   }, [page.activePage]);
 
+  // 계약관리 submit
+  const handleUseFlag = (e) => {
+    if (e.target.id === "useFlagY") {
+      for (let i = 0; i < clickedUseFlag.length; i++) {
+        servicesPostData(urlSetUser, {
+          uid: clickedUseFlag[i],
+          useFlag: "1",
+        }).then(window.location.reload());
+      }
+    } else {
+      for (let i = 0; i < clickedUseFlag.length; i++) {
+        servicesPostData(urlSetUser, {
+          uid: clickedUseFlag[i],
+          useFlag: "0",
+        }).then(window.location.reload());
+      }
+    }
+  };
+  console.log(clickedUseFlag);
+
   return (
     <>
       <ComponentListUserSearch
@@ -108,6 +101,12 @@ export default function ListUser() {
         setListPage={setListPage}
       />
       <ul className="tableTopWrap">
+        {clickedUseFlag.length > 0 && (
+          <LayoutTopButton text="정상" fn={handleUseFlag} id="useFlagY" />
+        )}
+        {clickedUseFlag.length > 0 && (
+          <LayoutTopButton text="해지" fn={handleUseFlag} id="useFlagN" />
+        )}
         <LayoutTopButton url="/adduser" text="회원 추가" />
       </ul>
       <section className="tableWrap">
@@ -116,6 +115,7 @@ export default function ListUser() {
           <table className="commonTable">
             <thead>
               <tr>
+                <th style={{ width: "65px" }}>계약관리</th>
                 <th style={{ width: "100px" }}>관리번호</th>
                 <th style={{ width: "250px" }}>아이디</th>
                 <th style={{ width: "150px" }}>이름</th>
@@ -130,29 +130,12 @@ export default function ListUser() {
             <tbody>
               {userList &&
                 userList.map((item) => (
-                  <tr key={item.uid}>
-                    <td>{item.uid}</td>
-                    <td>{item.userid}</td>
-                    <td>{item.name}</td>
-                    <td>
-                      {item.userrole && item.userrole.includes("ROLE_ADMIN")
-                        ? "관리자"
-                        : "일반"}
-                    </td>
-                    <td>{item.mobile}</td>
-                    <td>{item.createTime && item.createTime.slice(0, 10)}</td>
-                    <td>
-                      {item.udid ? <i className="tableIcon">입력</i> : null}
-                    </td>
-                    <td>
-                      {item.cid ? <i className="tableIcon">사업자</i> : null}
-                    </td>
-                    <td className="tableButton">
-                      <Link to={`${item.uid}`} className="Link">
-                        상세
-                      </Link>
-                    </td>
-                  </tr>
+                  <ListInTr
+                    item={item}
+                    key={item.uid}
+                    clickedUseFlag={clickedUseFlag}
+                    setClickedUseFlag={setClickedUseFlag}
+                  />
                 ))}
             </tbody>
           </table>
